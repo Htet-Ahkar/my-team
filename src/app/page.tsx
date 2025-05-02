d1a15a3d38ea184425c4511c452614ca074dea33
Xero <juliuscaesar.caesar37@gmail.com>
2025-05-02 11:24:01 +0700

    setup redux toolkit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
+"use client";
+
+import { decrement, increment } from "@/store/user/user-slice";
+import { useSelector, useDispatch } from "react-redux";
+
 export default function Home() {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const count = useSelector((state: any) => state.counter.value);
+  const dispatch = useDispatch();
   return (
     <section className="size-screen flex items-center justify-center">
-      Hello
+      <h1>Counter: {count}</h1>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
     </section>
   );
 }
